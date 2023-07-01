624329014407500106ea56ec647130132a8978f4
Parker Snell <parkersnell@gmail.com>
2023-07-01 13:36:21 -0700

    Annotate host interface functions with cdecl callconv

--- Sonora/HostInterface.cs
+++ Sonora/HostInterface.cs
@@ -10,28 +10,66 @@ public struct HostInterface
 
     public readonly bool IsValid => IsHosted != null;
 
+    #region Is hosted
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate bool FnIsHosted();
 
+    #endregion
+
+    #region Wave
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate nint FnWaveNewFromFile(
         [MarshalAs(UnmanagedType.LPUTF8Str)] string filename);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnWaveFree(nint wave);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate ulong FnWaveGetFrames(nint wave);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate uint FnWaveGetChannels(nint wave);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate ulong FnWaveReadFrames(
         nint wave,
         [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] float[] data,
         ulong size);
 
+    #endregion
+
+    #region Audio context
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate nint FnAudioContextNew();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnAudioContextFree(nint ctx);
 
+    #endregion
+
+    #region Device enumeration
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate nint FnDeviceEnumeratorNew(nint ctx);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnDeviceEnumeratorFree(nint devEnum);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate uint FnDeviceEnumeratorCount(nint devEnum, bool playback);
+
     // Returns a string but marshalling tries to free it, so it must be converted manually
     // via Marshal.PtrToStringUTF8.
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate nint FnDeviceEnumeratorGetDeviceName(
         nint devEnum, bool playback, uint index);
+
+    #endregion
+
+    #region Device
 
     // Must match ma_format in miniaudio.h
     public enum AudioFormat
@@ -43,13 +81,23 @@ public struct HostInterface
         S32     = 4,
         F32     = 5,
     }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate nint FnDeviceNew(
         nint ctx, nint devEnum, bool playback, uint index,
         [MarshalAs(UnmanagedType.I4)] AudioFormat format,
         uint channels, uint sampleRate);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnDeviceFree(nint device);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnDeviceStart(nint device);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void FnDeviceStop(nint device);
+
+    #endregion
 
     public FnIsHosted IsHosted;
 
