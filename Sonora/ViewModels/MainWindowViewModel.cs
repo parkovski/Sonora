913c662943e8235b187884af3cd0ee124f90cc36
Parker Snell <parkersnell@gmail.com>
2023-07-02 15:27:23 -0700

    Temp - large wave view & oscillator controls

--- Sonora/ViewModels/MainWindowViewModel.cs
+++ Sonora/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,24 @@
+using Sonora.Models;
+
 namespace Sonora.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public FilesViewModel Files { get; }
 
+    private Wave _wave;
+    public Wave Wave
+    {
+        get => _wave;
+        set => this.RaiseAndSetIfChanged(ref _wave, value);
+    }
+
     public MainWindowViewModel()
     {
         Files = new FilesViewModel();
+        _wave = new();
+
+        Files.WhenAnyValue(x => x.SelectedFile)
+            .Subscribe(x => Wave = x == null ? new Wave() : new Wave(x));
     }
 }
--- Sonora/Views/MainWindow.axaml.cs
+++ Sonora/Views/MainWindow.axaml.cs
@@ -5,8 +5,40 @@ namespace Sonora.Views;
 
 public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
+    nint _auctx;
+    nint _devEnum;
+    nint _dev;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        StartBtn.Click += StartBtn_Click;
+        StopBtn.Click += StopBtn_Click;
+        SineBtn.Click += (sender, e) => HostInterface.Instance.DeviceSetOsc(_dev, 0);
+        SawBtn.Click += (sender, e) => HostInterface.Instance.DeviceSetOsc(_dev, 1);
+        SquareBtn.Click += (sender, e) => HostInterface.Instance.DeviceSetOsc(_dev, 2);
+        TriangleBtn.Click += (sender, e) => HostInterface.Instance.DeviceSetOsc(_dev, 3);
+    }
+
+    private void StartBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (_auctx != 0) return;
+
+        ref readonly var host = ref HostInterface.Instance;
+        _auctx = host.AudioContextNew();
+        _devEnum = host.DeviceEnumeratorNew(_auctx);
+        _dev = host.DeviceNew(_auctx, _devEnum, true, 2, HostInterface.AudioFormat.F32, 2, 48000);
+        host.DeviceStart(_dev);
+    }
+
+    private void StopBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (_auctx == 0) return;
+        ref readonly var host = ref HostInterface.Instance;
+        host.DeviceFree(_dev);
+        host.DeviceEnumeratorFree(_devEnum);
+        host.AudioContextFree(_auctx);
+        _auctx = _devEnum = _dev = 0;
     }
 }
